e9dbef066fb2afe747eae80218216abec01aa8f8
Flora Lê <flora.mt.le@hotmail.com>
2022-04-21 11:23:03 +0200

    First commit of react app from my phone!

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Banner from './Banner'
+import Cart from './Cart'
+import '../App.css';
+
+function App() {
+  return (<div>
+    <Banner/>
+     <Cart/>
+     </div>
+  );
+}
+
+export default App;
