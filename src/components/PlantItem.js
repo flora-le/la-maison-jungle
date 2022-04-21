55a82a221a15eb85f4873f070c7c86b5ac4ccfbe
Flora Lê <flora.mt.le@hotmail.com>
2022-04-21 21:57:26 +0200

    img tag missing

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -4,7 +4,7 @@ import '../styles/PlantItem.css'
 function PlantItem({name, cover,id,water,light,category,isBestSale,isSpecialOffer }){
   return (
  <li key={`${name}-${id}`} className='lmj-plant-item'>
-                src={cover} alt={`${name} cover`} className="lmj-plant-item-cover"/>
+               <img src={cover} alt={`${name} cover`} className="lmj-plant-item-cover" />
                       { name }
                       {(isBestSale || category === "classique") && <span>🔥</span>}
                                 {isSpecialOffer  && <div className='lmj-sales'>Soldes</div>}
