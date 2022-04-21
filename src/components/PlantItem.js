2cd3ec0c175a26793540b2fa7ede17a737d83f7b
Flora Lê <flora.mt.le@hotmail.com>
2022-04-21 20:07:16 +0200

    update img src

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -4,8 +4,9 @@ import '../styles/PlantItem.css'
 function PlantItem({name, cover,id,water,light,category,isBestSale,isSpecialOffer }){
   return (
  <li key={`${name}-${id}`} className='lmj-plant-item'>
-                <img src={cover} alt={`${name} cover`} className="lmj-plant-item-cover"/>            { name }     
-                  {(isBestSale || category === "classique") && <span>🔥</span>}
+                src={cover} alt={`${name} cover`} className="lmj-plant-item-cover"/>
+                      { name }
+                      {(isBestSale || category === "classique") && <span>🔥</span>}
                                 {isSpecialOffer  && <div className='lmj-sales'>Soldes</div>}
               <div>                  
               <CareScale careType='water' scaleValue={water} />
